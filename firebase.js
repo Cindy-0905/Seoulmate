24307a04ed77fbe0cc9a4df64513b3b2276767cc
Cindy-0905 <cindyeldamaty0@gmail.com>
2025-06-19 09:52:01 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,22 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-database.js";
+// js/firebase.js
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCfbVpBgh5Pt86suk_NStuUcvUBAlN61_M",
   authDomain: "seoulmate-552f8.firebaseapp.com",
   projectId: "seoulmate-552f8",
-  storageBucket: "seoulmate-552f8.appspot.com",
+  storageBucket: "seoulmate-552f8.firebasestorage.app",
   messagingSenderId: "454531820117",
   appId: "1:454531820117:web:ee97698d89c87f146842dc",
-  measurementId: "G-LBEJGDJEBS",
-  databaseURL: "https://seoulmate-552f8-default-rtdb.firebaseio.com"
+  measurementId: "G-LBEJGDJEBS"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getDatabase(app);
-
-export { auth, db };
+const analytics = getAnalytics(app);
